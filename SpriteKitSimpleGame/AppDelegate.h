0208c62aa15ea627c04446a405bbc59448cde5e5
stephangopaul <stephangopaul@gmail.com>
2014-03-22 21:29:24 +0400

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Stephan Gopaul on 22/03/2014.
+//  Copyright (c) 2014 Stephan Gopaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
